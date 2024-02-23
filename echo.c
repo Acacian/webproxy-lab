a0a36fcdbb651614d57348f0b657709ed42bf191
DOHA <koo9811@gmail.com>
2024-02-23 23:53:07 +0900

    echo server

--- echo.c
+++ echo.c
@@ -2,10 +2,10 @@
 
 void echo(int connfd){
     size_t n;
-    char buf[MAXLINE]
+    char buf[MAXLINE];
     rio_t rio;
 
-    rio_readinitb(&rio, connfd);
+    Rio_readinitb(&rio, connfd);
     while((n = Rio_readlineb(&rio,buf,MAXLINE)) != 0){
         printf("server received %d bytes\n" , (int)n);
         Rio_writen(connfd, buf , n);
